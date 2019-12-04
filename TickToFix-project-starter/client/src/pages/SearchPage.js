cbbfae0609d07570723dfcab4efa4222564bddf9
BASH <ahamedb1991@gmail.com>
2019-12-04 04:08:25 -0500

    updated api route

---
+++ TickToFix-project-starter/client/src/pages/SearchPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SearchPage(props) {
+  return (
+    <div>Complete this</div>
+  );
+}
+
+export default SearchPage;
--- TickToFix-project-starter/client/src/pages/ShowPostPage.js
+++ TickToFix-project-starter/client/src/pages/ShowPostPage.js
@@ -11,26 +11,49 @@ class ShowPostPage extends React.Component {
   }
 
   componentDidMount() {
-    const { id } = this.props.match.params;
-    fetch("/api/posts/"+id)
-      .then(res => res.json())
-      .then(post => {
-        this.setState({
-          post: <Post {...post} />,
-          loading: false,
+    const { id, ticketNum } = this.props.match.params;
+
+
+    console.log(id, ticketNum);
+
+    if (id !== null && id !== undefined) {
+      console.log('id', id);
+      fetch("/api/posts/" + id)
+        .then(res => res.json())
+        .then(post => {
+          this.setState({
+            post: <Post {...post} />,
+            loading: false,
+          });
+        })
+        .catch(err => {
+          this.setState({
+            notFound: true,
+          });
         });
-      })
-      .catch(err => {
-        this.setState({
-          notFound: true,
+
+    } else {
+      console.log('ticket', ticketNum);
+      fetch("/api/posts/ticketNumber/" + ticketNum)
+        .then(res => res.json())
+        .then(post => {
+          this.setState({
+            post: <Post {...post} />,
+            loading: false,
+          });
+        })
+        .catch(err => {
+          this.setState({
+            notFound: true,
+          });
         });
-      });
+    }
   }
 
 
   render() {
-    if(this.state.notFound) return <Redirect to="/" />;
-    if(this.state.loading) return <Loading />;
+    if (this.state.notFound) return <Redirect to="/" />;
+    if (this.state.loading) return <Loading />;
     return this.state.post;
   }
 }
