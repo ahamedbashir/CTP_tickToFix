365673fb5cfa1ef11f2d1115e0edd5801423aacd
BASH <ahamedb1991@gmail.com>
2019-12-08 03:42:35 -0500

    clean up

--- TickToFix-project-starter/client/src/pages/AboutUsPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AboutUsPage(props) {
-  return (
-    <div className="mt-5">A little info about us.</div>
-  );
-}
-
-export default AboutUsPage;
--- TickToFix-project-starter/client/src/pages/SearchPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SearchPage(props) {
-  return (
-    <div>Complete this</div>
-  );
-}
-
-export default SearchPage;
