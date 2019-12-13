c137b4a872d1cbf5edc9bb6ccfcb6cda8c4645c2
BASH <ahamedb1991@gmail.com>
2019-12-13 16:51:14 -0500

    update to Post view

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Post({ title, content, userName, contactNum, apt, severity, status, createdAt, updatedAt, id }) {
+function Post({ title, content, userName, contactNum, apt, severity, status, appointmentStatus, createdAt, updatedAt, id }) {
   return (
     <div className="col-10 col-md-8 col-lg-7 mt-5">
       <div className="card mb-4 shadow">
-        <div className="card-title">
-          <Link to={"/posts/" + id}>{title} Reported from {apt}
+        <div className="card-title card-header">
+          <Link to={"/posts/" + id}><em> {title}</em> Reported from Apartment <em>{apt}</em>
           </Link>
         </div>
         <div className="card-body card-text">
-          {content}{severity} {status}
+          {content}
         </div>
         <div className="card-footer small text-muted text-right">
           <div>Reported at {createdAt}</div>
           <div>Last Updated at {updatedAt}</div>
+        </div>
+        <div className="container card bg-info text-white">
+          <div class="row">
+            <div class="col-sm">Severity: {severity}</div>
+            <div class="col-sm">Ticket Status: {status}</div>
+            <div class="col-sm">Appointment Status: {appointmentStatus}</div>
+          </div>
         </div>
       </div>
     </div>
